96fc797e8ccb809a8259e1f119eba6eb746fdfb2
Konrad Jakubowski <jakubowski.info@gmail.com>
2018-10-28 15:35:11 +0100

    Add bottom section in desktop resolution

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/*------- Smooth Scroll -------*/
+
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
+
+/*------- Swiper Slider -------*/
+       var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        centeredSlides: true,
+        autoplay: 10000,
+           speed: 1500,
+           loop: true,
+        autoplayDisableOnInteraction: false
+    });
